326a6d7461e21cea08aff0eac291b47de5d5bec4
p.andziul <ruri0000@gmail.com>
2016-05-06 17:29:16 +0200

    base

---
+++ src/application/Main.java
@@ -0,0 +1,45 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private Stage primaryStage;
+	private BorderPane mainFrame;
+
+	public static void main(String[] args)
+	{
+		launch(args);
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Interfejs desktopowy zarządzania hotelem");
+
+		try
+		{
+			FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(Main.class.getResource("view/MainWindow.fxml"));
+	        mainFrame = (BorderPane) loader.load();
+
+			Scene scene = new Scene(mainFrame);
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/application/controller/MainWindowController.java
@@ -0,0 +1,122 @@
+package application.controller;
+
+import java.io.IOException;
+
+import application.Main;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class MainWindowController {
+	
+	@FXML
+	private AnchorPane contentFrame;
+	
+	@FXML
+	private Button buttonLoadData;
+	
+	@FXML
+	private Button buttonInsertParameters;
+	
+	@FXML
+	private Button buttonStartAlgorithm;
+	
+	@FXML
+	private Button buttonShowResults;
+	
+	@FXML
+	private MenuItem menuButtonExit;
+	
+	@FXML
+	private void initialize()
+	{
+		loadAsPanel("view/StartWindow.fxml");
+	}
+	
+	public MainWindowController() 
+	{
+		
+	}
+	
+	// load window as panel ("view/win.fxml")
+	private void loadAsPanel(String resourcePath)
+	{
+		try
+		{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource(resourcePath));
+			AnchorPane window = (AnchorPane) loader.load();
+			contentFrame.getChildren().clear();
+			contentFrame.getChildren().add(window);
+			AnchorPane.setTopAnchor(window, 5.0);
+			AnchorPane.setBottomAnchor(window, 5.0);
+			AnchorPane.setLeftAnchor(window, 5.0);
+			AnchorPane.setRightAnchor(window, 5.0);
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+
+	// load as new window ("../view/win.fxml")
+	private void loadAsWindow(String resourcePath, String title)
+	{
+		try {
+			Stage stage;
+			Parent root;
+
+			stage = new Stage();
+
+			root = FXMLLoader.load(getClass().getResource(resourcePath));
+
+			stage.setScene(new Scene(root));
+			stage.setTitle(title);
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.showAndWait();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	private void buttonLoadDataClicked()
+	{
+		System.out.println("buttonLoadDataClicked");
+	}
+
+	@FXML
+	private void buttonInsertParametersClicked()
+	{
+		System.out.println("buttonInsertParametersClicked");
+	}
+
+	@FXML
+	private void buttonStartAlgorithmClicked()
+	{
+		System.out.println("buttonStartAlgorithmClicked");
+	}
+	
+	@FXML
+	private void buttonShowResultsClicked()
+	{
+		System.out.println("buttonShowResultsClicked");
+	}
+	@FXML
+	private void menuButtonExitClicked()
+	{
+		System.exit(0);
+	}
+	
+}
+
